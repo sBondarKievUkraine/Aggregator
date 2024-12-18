767343b7c49f68567c5f09f87a0cc916690c7626
Serhii.Bondar <serhii.bondar@codeminers.com.ua>
2024-12-19 00:55:26 +0200

    implement testcontainers test

--- src/test/java/com/company/aggregator/UserControllerIntegrationTest.java
+++ src/test/java/com/company/aggregator/UserControllerIntegrationTest.java
@@ -1,15 +1,19 @@
 package com.company.aggregator;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 
 @Testcontainers
 @ActiveProfiles("integration-test")
@@ -22,22 +26,26 @@ public class UserControllerIntegrationTest {
     @Autowired
     private UserController userController;
 
-
-    @Container
-    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.2")
-            .withDatabaseName("database1")
-            .withUsername("testuser")
-            .withPassword("testpass")
-            .withInitScript("init-schema.sql");
+    @BeforeEach
+    public void setUp() {
+        dataSourceConfig.getSources().forEach(source -> {
+            final var container = new PostgreSQLContainer<>(source.getStrategy())
+                    .withDatabaseName(source.getName())
+                    .withUsername(source.getUser())
+                    .withPassword(source.getPassword())
+                    .withInitScript(source.getUrl());
+            container.start();
+            assertThat(container.isRunning()).isTrue();
+            source.setUrl(container.getJdbcUrl());
+        });
+    }
 
     @Test
-    void testGetUsers() throws Exception {
-        assertThat(postgres.isRunning()).isTrue();
+    public void testGetUsers() throws Exception {
 
-        userController.getUsers();
-
-
-//        final ResultActions resultActions = mockMvc.perform(get("/api/users"))
+        final Collection<User> users = userController.getUsers();
+        System.out.println(users);
+//        final ResultActions resultActions = mockMvc.perform(get("/users"))
 //                .andExpect(status().isOk())
 //                .andExpect(jsonPath("$.name").value("John"))
 //                .andExpect(jsonPath("$.username").value("jdoe"));
